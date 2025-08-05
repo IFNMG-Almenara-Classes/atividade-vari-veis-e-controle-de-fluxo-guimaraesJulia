38f0a19fe88263a8c4693a8cf0dc5dc26f474c20
guimaraesJulia <guimaraesjulia979@gmail.com>
2025-08-05 20:19:19 -0300

    Resolução da questão 5.

---
+++ src/questao06/Principal.java
@@ -0,0 +1,11 @@
+package questao06;
+import java.util.Scanner;
+
+public class Principal{
+    public static void main(String[]grs){
+        int frt;
+        Scanner ler = new Scanner(System.in);
+        frt = ler.nextInt();
+
+    }
+}
